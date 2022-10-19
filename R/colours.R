5f0dcbf7ef75084a563a3c98d326825db2dd7de5
dave-lovell <davidpg.lovell@gmail.com>
2022-10-19 16:38:15 +0100

    adding all colours

--- R/colours.R
+++ R/colours.R
@@ -1,14 +1,42 @@
-## Church Army Colours
+# Church Army Colours #
 
+## Wrappers & Helpers ###############################################################################
+
+
+ca_pallete <- function(pallette){
+
+  if(is.function(palette)) palette <- palette()
+
+  rlang::new_function(
+    args = rlang::pairlist2(x = 1),
+    body =
+      quote({
+        n_pal <- length(palette)
+        stopifnot(all(x <= n_pal), all(x > 0))
+
+        out_col <- palette[x]
+        return(out_col)
+        }))}
+
+## PRIMARY #########################################################################################
+
+### Black  ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ca_black <- function() "#000000"
+ca_white <- function() "#FFFFFF"
 
-ca_greys <- function() c("#262626", "#595959", "#8c8c8c", "#bfbfbf", "d9d9d9", "#e5e5e5", "#f2f2f2")
+## SECONDARY ###### ################################################################################
 
-ca_grey <- function(k = c("85", "65", "45", "25", "15", "10", "5")){
-  k = match.arg(k)
-  k.index <- which(c("85", "65", "45", "25", "15", "10", "5") == k)
+ca_orange <- function() "#E84619"
+ca_lime   <- function() "#E1DE00"
+ca_gold   <- function() "#FBC900"
+ca_green  <- function() "#509E2F"
+ca_maroon <- function() "#910048"
+ca_cyan   <- function() "#0092BC"
 
-  grey <- ca_greys()[k.index]
+## TERTIARY #########################################################################################
 
-  grey
-}
+ca_light_teal <- function() "#6C8B93"
+ca_dark_teal  <- function() "#006272"
+ca_brown      <- function() "#6F2C3F"
+
+## PALETTES #########################################################################################
