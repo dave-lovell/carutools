1258d1f1cf0a5d6be853b8e377787689e50de7b9
dave-lovell <davidpg.lovell@gmail.com>
2022-10-19 14:23:18 +0100

    renaming functions (ca_prefix)

--- R/colours.R
+++ R/colours.R
@@ -1,10 +1,10 @@
 ## Church Army Colours
 
-ct_black <- function() "#000000"
+ca_black <- function() "#000000"
 
-ct_greys <- function() c("#262626", "#595959", "#8c8c8c", "#bfbfbf", "d9d9d9", "#e5e5e5", "#f2f2f2")
+ca_greys <- function() c("#262626", "#595959", "#8c8c8c", "#bfbfbf", "d9d9d9", "#e5e5e5", "#f2f2f2")
 
-ct_grey <- function(k = c("85", "65", "45", "25", "15", "10", "5")){
+ca_grey <- function(k = c("85", "65", "45", "25", "15", "10", "5")){
   k = match.arg(k)
   k.index <- which(c("85", "65", "45", "25", "15", "10", "5") == k)
 
