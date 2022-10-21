9da3641f8eb0d5cf433a4779cf1620cb02e0a3f7
dave-lovell <davidpg.lovell@gmail.com>
2022-10-21 11:35:49 +0100

    exporting all colours functions

--- R/colours.R
+++ R/colours.R
@@ -21,22 +21,54 @@ ca_pallete <- function(pallette){
 ## PRIMARY #########################################################################################
 
 ### Black  ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+#' Church Army Colours
+#'
+#' These functions each return the hexadecimal code pertaining to a colour in Church Army's brand
+#'
+#' These functions are wrappers around character vectors of length one. They take no arguments and always return the same string.
+#'
+#' @returns
+#' A single character vector of length one.
+#' @export
+#' @rdname ca_cols
 ca_black <- function() "#000000"
+#' @rdname ca_cols
+#' @export
 ca_white <- function() "#FFFFFF"
 
 ## SECONDARY ###### ################################################################################
 
+#' @rdname ca_cols
+#' @export
 ca_orange <- function() "#E84619"
+#' @rdname ca_cols
+#' @export
 ca_lime   <- function() "#E1DE00"
+#' @rdname ca_cols
+#' @export
 ca_gold   <- function() "#FBC900"
+#' @rdname ca_cols
+#' @export
 ca_green  <- function() "#509E2F"
+#' @rdname ca_cols
+#' @export
 ca_maroon <- function() "#910048"
+#' @rdname ca_cols
+#' @export
 ca_cyan   <- function() "#0092BC"
-
-## TERTIARY #########################################################################################
-
+#' @rdname ca_cols
+#' @export
 ca_light_teal <- function() "#6C8B93"
+#' @rdname ca_cols
+#' @export
 ca_dark_teal  <- function() "#006272"
+#' @rdname ca_cols
+#' @export
 ca_brown      <- function() "#6F2C3F"
 
 ## PALETTES #########################################################################################
+#
+# oranges <- c("#E84619", "#ED6B47", "#F19074", "#F6B6A4", "#FADAD1")
+# golds   <- c("#FBC900", "#FCD446", "#FCDF74", "#FDE9A1", "#FEF4D0")
+# lighttea  <- c("#")
