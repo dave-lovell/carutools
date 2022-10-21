4050f03d829ca6e53fc134d0049dbcd421a440ae
dave-lovell <davidpg.lovell@gmail.com>
2022-10-21 12:09:03 +0100

    adding colour scales

--- R/colours.R
+++ R/colours.R
@@ -1,24 +1,5 @@
 # Church Army Colours #
 
-## Wrappers & Helpers ###############################################################################
-
-
-ca_pallete <- function(pallette){
-
-  if(is.function(palette)) palette <- palette()
-
-  rlang::new_function(
-    args = rlang::pairlist2(x = 1),
-    body =
-      quote({
-        n_pal <- length(palette)
-        stopifnot(all(x <= n_pal), all(x > 0))
-
-        out_col <- palette[x]
-        return(out_col)
-        }))}
-
-
 #' Church Army Colours
 #'
 #' These functions each return the hexadecimal code pertaining to a colour in Church Army's brand
@@ -62,7 +43,33 @@ ca_dark_teal  <- function() "#006272"
 ca_brown      <- function() "#6F2C3F"
 
 ## PALETTES #########################################################################################
-#
-# oranges <- c("#E84619", "#ED6B47", "#F19074", "#F6B6A4", "#FADAD1")
-# golds   <- c("#FBC900", "#FCD446", "#FCDF74", "#FDE9A1", "#FEF4D0")
-# lighttea  <- c("#")
+
+## Behind the scenes helper -------------------------------------------------------------------------
+
+ca_pallete <- function(pallette){
+
+  if(is.function(palette)) palette <- palette()
+
+  rlang::new_function(
+    args = rlang::pairlist2(x = 1),
+    body =
+      quote({
+        n_pal <- length(palette)
+        stopifnot(all(x <= n_pal), all(x > 0))
+
+        out_col <- palette[x]
+        return(out_col)
+      }))}
+# --------------------------------------------------------------------------------------------------
+# Pallettes proper ---------------------------------------------------------------------------------
+
+oranges      <- c("#E84619", "#ED6B47", "#F19074", "#F6B6A4", "#FADAD1")
+golds        <- c("#FBC900", "#FCD446", "#FCDF74", "#FDE9A1", "#FEF4D0")
+light_teals  <- c("#6C8B93", "#8AA2A9", "#A6B9BF", "#C4D1D3", "#E2E9E9")
+cyans        <- c("#0092BC", "#36AFDA", "#68C4E2", "#9AD6EB", "#CCE9F3")
+dark_teals   <- c("#006272", "#35818D", "#67A1AB", "#9AC0C8", "#CCDFE3")
+maroons      <- c("#9A054A", "#AE396F", "#C26A93", "#D89CBB", "#EBCED8")
+purples      <- c("#523178", "#765B94", "#9784AE", "#BBAEC9", "#DCD6E4")
+browns       <- c("#652430", "#85515A", "#A37C83", "#C1A7AD", "#E1D4D7")
+greens       <- c("#2AB04A", "#5EB66D", "#84C491", "#ACD6B5", "#D5E9D9")
+
