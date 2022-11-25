de7f822d18b4d967bc3266261380d4773b97f8bc
Dave Lovell <davidpg.lovell@gmail.com>
2022-11-25 16:43:09 +0000

    adding colour scale

--- R/ggplot-scales.R
+++ R/ggplot-scales.R
@@ -23,6 +23,9 @@ make_caru_ggpalette <- function(pal_name){
 
 ca_scale_fill_discrete <- function(ca_pallette = "mix", ...){
   ggplot2::scale_fill_discrete(type = ca_pal(ca_pallette), ...)
-  }
+}
+ca_scale_colour_discrete <- function(ca_palette = "mix", ...){
+  ggplot2::scale_colour_discrete(type = ca_palette, ...)
+}
 
 
