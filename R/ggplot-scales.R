2696373f613e01745ce365e639c11c80ed42d258
Dave Lovell <davidpg.lovell@gmail.com>
2022-11-25 16:44:34 +0000

    removing unnecessary function

--- R/ggplot-scales.R
+++ R/ggplot-scales.R
@@ -1,26 +1,3 @@
-## ggpallettes
-make_caru_ggpalette <- function(pal_name){
-
-  ## Get CA palette by name
-  pal <- ca_pal(pal_name)
-
-  ## Construct palette
-  palette <-
-    rlang::new_function(
-      rlang::exprs(n = ),
-      rlang::expr({
-        scale <- !!pal
-        index <- 1:n
-        out <- scale[index]
-        return(out)
-      }),
-      rlang::caller_env()
-    )
-
-  return(palette)
-}
-
-
 ca_scale_fill_discrete <- function(ca_pallette = "mix", ...){
   ggplot2::scale_fill_discrete(type = ca_pal(ca_pallette), ...)
 }
