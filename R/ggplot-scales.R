8f65b9667087e04b73165189f30bd93048e412f4
Dave Lovell <davidpg.lovell@gmail.com>
2022-11-25 16:36:13 +0000

    building discrete fill scale

--- R/ggplot-scales.R
+++ R/ggplot-scales.R
@@ -1,3 +1,28 @@
-ca_scale_fill_discrete <- function(){
+## ggpallettes
+make_caru_ggpalette <- function(pal_name){
 
+  ## Get CA palette by name
+  pal <- ca_pal(pal_name)
+
+  ## Construct palette
+  palette <-
+    rlang::new_function(
+      rlang::exprs(n = ),
+      rlang::expr({
+        scale <- !!pal
+        index <- 1:n
+        out <- scale[index]
+        return(out)
+      }),
+      rlang::caller_env()
+    )
+
+  return(palette)
 }
+
+
+ca_scale_fill_discrete <- function(ca_pallette = "orange", ...){
+  ggplot2::scale_fill_discrete(type = ca_pal(ca_pallette), ...)
+  }
+
+
